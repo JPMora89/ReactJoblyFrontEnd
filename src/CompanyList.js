5c353e084178c41bc751842cbf63f0fc26fd2e44
Pablo M <jpmora123@yahoo.com>
2023-05-21 14:13:33 -0400

    Erased commented-out trial code

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,38 +1,4 @@
-// import React, {useEffect, useState} from 'react';
-// import SearchForm from './SearchForm';
-// import JoblyApi from './api';
-// import CompanyCard from './CompanyCard';
 
-// function CompanyList() {
-//   const [companies, setCompanies] = useState([]);
-
-//   useEffect(() => {
-//     const getCompanies = async () => {
-//       const req = await JoblyApi.getAllCompanies();
-//       setCompanies(req);
-//     }
-//     getCompanies();
-//   }, []);
-
-//   const updateCompanies = (filteredCompanies) => {
-//     setCompanies([...filteredCompanies]);
-//   }
-
-//   return (
-//     <>
-//       <SearchForm updateCompanies={updateCompanies}/>
-//       <div>
-//         {companies.map(company => (
-//           <div>
-//             <CompanyCard company={company} />
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default CompanyList;
 
 import React, {useEffect, useState} from 'react';
 import SearchForm from './SearchForm';
@@ -65,18 +31,7 @@ function CompanyList() {
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-  // return (
-  //   <>
-  //     <SearchForm updateCompanies={updateCompanies}/>
-  //     <div>
-  //       {companies.map(company => (
-  //         <div>
-  //           <CompanyCard company={company} />
-  //         </div>
-  //       ))}
-  //     </div>
-  //   </>
-  // )
+
   return (
     <>
       <SearchForm updateCompanies={updateCompanies} />
